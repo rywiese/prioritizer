61d184d67d2db1140de954eccf3b219a68d3a494
Ry Wiese <ry.wiese@gmail.com>
2022-06-26 12:56:39 -0500

    Adds some component stubs (#3)

--- src/commonMain/kotlin/model/Tree.kt
+++ src/commonMain/kotlin/model/Tree.kt
@@ -2,10 +2,12 @@ package model
 
 import protocol.Identifiable
 
-interface PriorityTree : Identifiable {
+interface Tree : Identifiable {
 
     val node: Node
 
     val childIds: Set<String>
+
+    val queue: List<Item>
 
 }
