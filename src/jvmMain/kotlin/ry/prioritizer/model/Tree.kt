d98d0bb83df2166b36e274e491e22f4603070e50
Ry Wiese <ry.wiese@gmail.com>
2022-11-05 13:20:20 -0500

    Replace jackson with ktor (#20)

--- src/jvmMain/kotlin/ry/prioritizer/model/Tree.kt
+++ src/jvmMain/kotlin/ry/prioritizer/model/Tree.kt
@@ -1,8 +1,8 @@
 package ry.prioritizer.model
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.node.JsonNodeFactory
-import com.fasterxml.jackson.databind.node.ObjectNode
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
 
 interface Tree {
 
@@ -13,17 +13,12 @@ interface Tree {
     // TODO: Make this a Set.
     val children: List<Tree>
 
-    fun toJson(): JsonNode = JsonNodeFactory.instance.objectNode()
-        .set<ObjectNode>("category", category.toJson())
-        .set<ObjectNode>("queue", JsonNodeFactory.instance.arrayNode().apply {
-            queue.forEach { item: Item ->
-                add(item.toJson())
-            }
-        })
-        .set<ObjectNode>("children", JsonNodeFactory.instance.arrayNode().apply {
-            children.forEach { child: Tree ->
-                add(child.toJson())
-            }
-        })
+    fun toJson(): JsonElement = JsonObject(
+        mapOf(
+            "category" to category.toJson(),
+            "queue" to JsonArray(queue.map(Item::toJson)),
+            "children" to JsonArray(children.map(Tree::toJson))
+        )
+    )
 
 }
