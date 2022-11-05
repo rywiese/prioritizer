00aad9a58b02659866e26baef5232e77e0378329
rywiese <ry.wiese@gmail.com>
2022-11-05 13:00:24 -0500

    Http swap (#19)
    * Unplugs graphql and plugs in one http endpoint for getting root
    
    * Get tree by categoryId endpoint
    
    * graphql is gone

--- src/jvmMain/kotlin/ry/prioritizer/ktor/KtorModule.kt
+++ src/jvmMain/kotlin/ry/prioritizer/ktor/KtorModule.kt
@@ -5,7 +5,7 @@ import dagger.Provides
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import ry.prioritizer.ktor.plugins.GraphQLPlugin
+import ry.prioritizer.ktor.plugins.PrioritizerHttpPlugin
 import ry.prioritizer.ktor.plugins.WebGuiPlugin
 import javax.inject.Named
 import javax.inject.Singleton
@@ -19,11 +19,11 @@ object KtorModule {
         @Named("host") host: String,
         @Named("port") port: Int,
         webGuiPlugin: WebGuiPlugin,
-        graphQLPlugin: GraphQLPlugin,
+        httpPlugin: PrioritizerHttpPlugin
     ): ApplicationEngine =
         embeddedServer(Netty, port = port, host = host) {
             webGuiPlugin.applyPlugin(this)
-            graphQLPlugin.applyPlugin(this)
+            httpPlugin.applyPlugin(this)
         }
 
 }
--- src/jvmMain/kotlin/ry/prioritizer/model/Category.kt
+++ src/jvmMain/kotlin/ry/prioritizer/model/Category.kt
@@ -1,12 +1,16 @@
 package ry.prioritizer.model
 
-import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
 
-@GraphQLIgnore
 interface Category {
 
     val id: String
 
     val name: String
+
+    fun toJson(): JsonNode = JsonNodeFactory.instance.objectNode()
+        .put("id", id)
+        .put("name", name)
 
 }
