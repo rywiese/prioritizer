ae04865d1d1f2a602ab2b029ca03dc0223e1da9c
Ry Wiese <ry.wiese@gmail.com>
2022-11-08 00:42:54 -0600

    fixup to url and also an openapi yml file (#33)

--- src/jvmMain/kotlin/ry/prioritizer/ktor/KtorModule.kt
+++ src/jvmMain/kotlin/ry/prioritizer/ktor/KtorModule.kt
@@ -8,7 +8,7 @@ import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import ry.prioritizer.ktor.plugins.HttpPlugin
+import ry.prioritizer.ktor.plugins.RestPlugin
 import ry.prioritizer.ktor.plugins.WebGuiPlugin
 import javax.inject.Named
 import javax.inject.Singleton
@@ -22,14 +22,14 @@ object KtorModule {
         @Named("host") host: String,
         @Named("port") port: Int,
         webGuiPlugin: WebGuiPlugin,
-        httpPlugin: HttpPlugin
+        restPlugin: RestPlugin
     ): ApplicationEngine =
         embeddedServer(Netty, port = port, host = host) {
             install(ContentNegotiation) {
                 json()
             }
             webGuiPlugin.applyPlugin(this)
-            httpPlugin.applyPlugin(this)
+            restPlugin.applyPlugin(this)
         }
 
 }
